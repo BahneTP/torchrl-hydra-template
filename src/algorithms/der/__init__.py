1dfae7eb5d38afa60b17239ed0ee98bb5fe7e2ca
Raphael Schwinger <raphaelschwinger@gmail.com>
2026-07-16 10:43:02 +0000

    Refator to use torchrl components

--- src/algorithms/der/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.algorithms.der.der import DERAgent
-from src.algorithms.der.der import DERConfig
-
-__all__ = ["DERAgent", "DERConfig"]
--- src/replay/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.replay.prioritized import PrioritizedSubsequenceReplayBuffer
-
-__all__ = ["PrioritizedSubsequenceReplayBuffer"]
